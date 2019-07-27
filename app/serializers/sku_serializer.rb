eec372d6dfacd1b8a6b71bbfd0f7a79cd802c19d
Gabriel Vargas <gabriel.vargas@continuodev.com.br>
2019-07-27 13:23:55 -0300

    listagem dos skus

--- app/serializers/sku_serializer.rb
+++ app/serializers/sku_serializer.rb
@@ -1,11 +1,16 @@
 class SkuSerializer < ActiveModel::Serializer
   attributes :id, :code, :hardware_type, :manufacturer, :model, :ram_memory,
-             :archive, :damages
+             :archive, :damages, :category, :processor, :disk_size, :disk_type, :webcam, :keyboard_type
 
   has_one :hardware_type
   has_one :manufacturer
   has_one :model
   has_one :archive
+  has_one :category
+  has_one :processor
+  has_one :disk_size
+  has_one :disk_type
+  has_one :keyboard_type
 
   def hardware_type
     return object.hardware_type.name
@@ -20,7 +25,11 @@ class SkuSerializer < ActiveModel::Serializer
   end
 
   def category
-    return object.category.name
+    unless object.category.nil?
+      return object.category.name
+    else
+      return ''
+    end
   end
 
   def damages
@@ -31,6 +40,14 @@ class SkuSerializer < ActiveModel::Serializer
     return dams
   end
 
+  def processor
+    unless object.processor.nil?
+      return object.processor.name
+    else
+      return ''
+    end
+  end
+
   def archive
     if object.archive.attached?
       url = 'http://'+ ENV["url"] + Rails.application.routes.url_helpers.rails_blob_path( object.archive, disposition: "attachment" )
@@ -39,4 +56,40 @@ class SkuSerializer < ActiveModel::Serializer
       return 'false'
     end
   end
+
+  def disk_size
+    unless object.disk_size.nil?
+      return object.disk_size.name
+    else
+      return ''
+    end
+  end
+
+  def disk_type
+    unless object.disk_size.nil?
+      return object.disk_size.name
+    else
+      return ''
+    end
+  end
+
+  def webcam
+    case object.webcam
+    when '12'
+      return 'Sim'
+    when '13'
+      return 'Não'
+    else
+      return ' '
+    end
+  end
+
+  def keyboard_type
+    unless object.keyboard_type.nil?
+      return object.keyboard_type.name
+    else
+      return ''
+    end
+  end
+
 end
